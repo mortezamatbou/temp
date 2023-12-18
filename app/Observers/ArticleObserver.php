451196cff7b916f1ed3227baf779ddcb941d1aa0
Morteza Matbou <mortezamatbou91@gmail.com>
2023-12-18 14:45:16 +0330

    implement simple logging with model observer. it can be used as facade

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -19,7 +19,33 @@ class ArticleObserver
      */
     public function updated(Article $article): void
     {
-        //
+        $properties = [];
+
+        if ($article->isDirty('title')) {
+            $properties['attributes']['title'] = $article->title;
+            $properties['old']['title'] = $article->getOriginal('title');
+        }
+        if ($article->isDirty('status_id')) {
+            $properties['attributes']['status_id'] = $article->status_id;
+            $properties['old']['status_id'] = $article->getOriginal('status_id');
+        }
+        if ($article->isDirty('slug')) {
+            $properties['attributes']['slug'] = $article->slug;
+            $properties['old']['slug'] = $article->getOriginal('slug');
+        }
+        if ($article->isDirty('body')) {
+            $properties['attributes']['body'] = $article->body;
+            $properties['old']['body'] = $article->getOriginal('body');
+        }
+
+        if ($properties) {
+            activity('article-observer')
+                ->performedOn($article)
+                ->causedBy(auth()->id())
+                ->withProperties($properties)
+                ->log('article updated.');
+        }
+
     }
 
     /**
